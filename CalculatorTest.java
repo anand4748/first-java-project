239b2dce430f159a83f59e8e9593aa802ceebd1f
anand4748 <anandbhosale771@gmail.com>
2025-01-30 22:01:26 +0530

    feature of added into calculatot

---
+++ CalculatorTest.java
@@ -0,0 +1,16 @@
+class Calculator {
+
+    public int add(int a,int b){
+        return a+b;
+    }
+}
+
+public class CalculatorTest {
+
+
+
+    public static void main(String[]args){
+Calculator cal= new Calculator();
+System.out.println("Result:"+ cal.add(10,20));
+    }
+    }
